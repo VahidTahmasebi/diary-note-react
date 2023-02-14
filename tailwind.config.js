59c1906824d479fe5579b824d52e67e3c2d53f81
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-14 20:48:02 +0330

    Add css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx,vue}'],
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': 'var(--primary-color)',
+        'primary-color-hover': 'var(--primary-color-hover)',
+        'secondary-color': 'var(--secondary-color)',
+        'main-white': 'var(--main-white)',
+        'main-black': 'var(--main-black)',
+        'main-grey': 'var(--main-grey)',
+        'mein-orange': 'var(--mein-orange)',
+        'body-color': 'var(--body-color)',
+      },
+    },
   },
   plugins: [],
 };
