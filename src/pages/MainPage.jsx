844bf2c9f292fd590467677683ff7a366214ec28
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-14 20:49:45 +0330

    Add style to mainPage file

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,13 +4,19 @@ import '../assets/styles/index.css';
 
 const MainPage = () => {
   return (
-    <section className=''>
-      <h2>Daily notes</h2>
-      <p>Take notes, reminders, set targets</p>
-      <Link to='/new-note'>
-        <button>Get Started</button>
-      </Link>
-      <p>welcome user</p>
+    <section className='flex flex-col w-full h-screen overflow-auto justify-center items-center'>
+      <div className='text-center my-auto'>
+        <h1 className='text-3xl mb-10'>Daily notes</h1>
+        <p className=''>Take notes, reminders, set targets</p>
+      </div>
+      <div className='text-center my-auto'>
+        <Link to='/new-note'>
+          <button className='py-4 px-4 mb-2 rounded-full bg-primary-color hover:bg-primary-color-hover focus:opacity-70 focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-200 transition ease-in duration-200 text-base font-semibold shadow-md outline-none'>
+            Get Started
+          </button>
+        </Link>
+        <p>welcome user - dynamic</p>
+      </div>
     </section>
   );
 };
