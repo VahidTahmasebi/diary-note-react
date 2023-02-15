98b849a31cb1877ad1a70ee49c8a0d586754a68e
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-15 21:06:43 +0330

    loop over notes

--- src/pages/NotesListPage.jsx
+++ src/pages/NotesListPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import NoteItem from '../components/NoteItem';
 import Layout from '../Layout/Layout';
 
 const NotesListPage = () => {
+  const { notes, loading, error } = useSelector((state) => state.notes);
+
+  if (loading) return <p>loading ...</p>;
+  if (error) return <p>error ...</p>;
+
   return (
     <section className='h-screen flex flex-col justify-start items-center'>
       <Layout>
-        <div>NotesListPage</div>
+        <div>
+          <ul>
+            {notes.map((note) => (
+              <NoteItem id={note.id} {...note} />
+            ))}
+          </ul>
+        </div>
       </Layout>
     </section>
   );
