69babfabab100ddbbbd0048777aed09b679ad785
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-23 04:39:10 +0330

    Fix type

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -6,7 +6,7 @@ const NoteItem = ({ ...note }) => {
 
   return (
     <>
-      <Link to={`user/${id}`} state={{ note }}>
+      <Link to={`/user/${id}`} state={{ note }}>
         <li className='h-20 w-full py-6 px-5 my-3 bg-main-grey rounded-2xl'>
           <div className='flex items-center'>
             <span>{subject}</span>
