bae65f2c6a478bcf3b4f7315782c1aaa50d9a83c
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-02-28 17:51:37 +0330

    Add checklist style

--- src/pages/FormNotePage.jsx
+++ src/pages/FormNotePage.jsx
@@ -23,6 +23,7 @@ const FormNotePage = () => {
     progressModal: false,
     datesModal: false,
     placeModal: false,
+    checkListModal: false,
   });
 
   const dispatch = useDispatch();
@@ -181,8 +182,65 @@ const FormNotePage = () => {
                 onChange={changeHandler}
               />
             </div>
-            <div>
-              <label className='opacity-70'>todo</label>
+
+            {/* checklist form */}
+            <div className='lg:w-5/6'>
+              <label className='opacity-70'>Checklist</label>
+              <ul className='flex flex-col lg:w-full h-fit p-3 pt-3 rounded-xl bg-gray-700 shadow-lg transition ease-in duration-200 overflow-y-auto'>
+                <div className='flex mb-4'>
+                  <input
+                    type='checklistValue'
+                    id='checklistValue'
+                    name='checklistValue'
+                    // value={inputChecklist}
+                    // onChange={(e)=>}
+                    placeholder='your check...'
+                    className='py-2 px-3 w-11/12 text-main-black rounded-l-xl outline-none shadow-lg focus:ring-1 focus:ring-offset-1 focus:ring-indigo-200 transition ease-in duration-200'
+                  />
+                  <button
+                    type='submit'
+                    onClick={addNewTagHandler}
+                    className='w-2/12 py-2 px-1 rounded-r-xl bg-primary-color hover:bg-primary-color-hover focus-within:opacity-70 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-indigo-200 text-sm font-semibold shadow-md  transition ease-in duration-200 outline-none'
+                  >
+                    Add
+                  </button>
+                </div>
+                <li className='flex items-center lg:w-full h-fit max-h-20 mb-2 rounded-xl shadow-lg hover:ring-1 hover:ring-offset-1 hover:ring-indigo-200 transition ease-in duration-200'>
+                  <label
+                    htmlFor='checklist'
+                    className='flex px-5 py-3 h-full items-center overflow-hidden overflow-x-auto flex-1 cursor-pointer'
+                  >
+                    <input
+                      type='checkbox'
+                      id='checklist'
+                      name='checklist'
+                      className='opacity-0 absolute cursor-pointer'
+                    />
+                    <div className='bg-white border-2 rounded-md border-primary-color w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 focus-within:border-primary-color'>
+                      <svg
+                        className='fill-current hidden w-2 h-2 text-primary-color pointer-events-none'
+                        version='1.1'
+                        viewBox='0 0 17 12'
+                        xmlns='http://www.w3.org/2000/svg'
+                      >
+                        <g fill='none' fillRule='evenodd'>
+                          <g
+                            transform='translate(-9 -11)'
+                            fill='#1F73F1'
+                            fillRule='nonzero'
+                          >
+                            <path d='m25.576 11.414c0.56558 0.55188 0.56558 1.4439 0 1.9961l-9.404 9.176c-0.28213 0.27529-0.65247 0.41385-1.0228 0.41385-0.37034 0-0.74068-0.13855-1.0228-0.41385l-4.7019-4.588c-0.56584-0.55188-0.56584-1.4442 0-1.9961 0.56558-0.55214 1.4798-0.55214 2.0456 0l3.679 3.5899 8.3812-8.1779c0.56558-0.55214 1.4798-0.55214 2.0456 0z' />
+                          </g>
+                        </g>
+                      </svg>
+                    </div>
+                    <span className='overflow-x-auto'>
+                      WWWWWWWWWWWWWWWWWWWWWW
+                    </span>
+                  </label>
+                  <span className='px-5 cursor-pointer'>delete</span>
+                </li>
+              </ul>
             </div>
           </div>
 
@@ -275,7 +333,12 @@ const FormNotePage = () => {
                 </li>
 
                 {/* checklist */}
-                <li className='flex'>
+                <li
+                  className='flex cursor-pointer'
+                  onClick={() =>
+                    setModalState({ ...modalState, checkListModal: true })
+                  }
+                >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
                     fill='none'
