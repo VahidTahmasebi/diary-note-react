26fcf6b36072d0f40f1e7cd109cfcb9fbfd4ae64
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2023-03-07 16:45:18 +0330

    Add input common

---
+++ src/common/Input.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Input = ({ label, name, formik, type = 'text' }) => {
+  return (
+    <div className='lg:w-2/12 flex flex-col justify-between'>
+      <label htmlFor={name} className='opacity-70'>
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        {...formik.getFieldProps(name)}
+        className='w-full p-2 text-main-black rounded-xl outline-none shadow-lg focus:ring-1 focus:ring-offset-1 focus:ring-indigo-200 transition ease-in duration-200'
+      />
+      <div className='h-4 pt-1'>
+        {formik.errors[name] && formik.touched[name] && (
+          <div className='text-red-300'>{formik.errors[name]}</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Input;
